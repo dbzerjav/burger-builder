bdbba0f5ab21d3932c339f3c04619a6c497f429a
dan Z <dbzerjav@gmail.com>
2019-02-08 01:05:23 -0600

    build burger builder

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+const layout = ( props ) => (
+  <>
+    <Toolbar />
+    <main className={classes.Content}>
+      {props.children}
+    </main>
+  </>
+);
+
+export default layout;
